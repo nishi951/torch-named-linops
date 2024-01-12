b8d247574ad367358285cce50cb23e69b63cd3cd
Mark Nishimura <nishimuramarky@gmail.com>
2024-01-11 17:42:07 -0800

    Adding dense and putting in alphabetical order

--- src/torchlinops/core/linops.py
+++ src/torchlinops/core/linops.py
@@ -1,10 +1,18 @@
 import torch
+from einops import einsum
 
 from .base import NamedLinop
+
+__all__ = [
+    'Dense',
+    'Diagonal',
+    'Repeat',
+]
 
 class Broadcast(NamedLinop):
     """Return a rearrange matching batched ishape to oshape.
     Basically broadcast to each other
+    TODO: Fix this class
     """
     def __init__(self, ishape, oshape):
         super().__init__(ishape, oshape)
@@ -14,22 +22,102 @@ class Broadcast(NamedLinop):
     def forward(self, x):
         return self.fn(x, self.ishape_str, self.oshape_str)
 
-    @classmethod
     def fn(cls, x, ishape_str, oshape_str):
         return rearrange(x, f'... {ishape_str} -> ... {oshape_str}')
 
-    @classmethod
     def adj_fn(cls, x: torch.Tensor, ishape_str, oshape_str):
         return rearrange(x, f'... {oshape_str} -> ... {ishape_str}')
 
-    def _split(self, ibatch, obatch):
+    def split(self, ibatch, obatch):
         return self # Literally don't change anything
+
+class Dense(NamedLinop):
+    """
+    Example:
+    x: [A, Nx, Ny]
+    weightshape: [A, T]
+    oshape: [T, Nx, Ny]
+    """
+    def __init__(self, weight, weightshape, ishape, oshape):
+        super().__init__(ishape, oshape)
+        self.weight = weight
+        self.einstr = f'{" ".join(self.ishape)},{" ".join(self.weightshape)}->{" ".join(self.oshape)}'
+        self.adj_einstr = f'{" ".join(self.oshape)},{" ".join(self.weightshape)}->{" ".join(self.ishape)}'
+
+    def fn(self, x, /, weight):
+        return einsum(x, weight, self.einstr)
+
+    def adj_fn(self, x, /, weight):
+        return einsum(x, weight, self.adj_einstr)
+
+    def normal_fn(self, x, /, weight):
+        return self.adj_fn(self.fn(x, weight), weight)
+
+    def split_forward(self, ibatch, obatch):
+        weight = self.split_forward_fn(ibatch, obatch, self.weight)
+        return type(self)(weight, self.weightshape, self.ishape, self.oshape)
+
+    def split_forward_fn(self, ibatch, obatch, /, weight):
+        weightbatch = [slice(None)] * len(self.weightshape)
+        for dim, batch in zip(self.ishape, ibatch):
+            if dim in self.weightshape:
+                weightbatch[self.weightshape.index(dim)] = batch
+        for dim, batch in zip(self.oshape, ibatch):
+            if dim in self.weightshape:
+                weightbatch[self.weightshape.index(dim)] = batch
+        return weight[weightbatch]
+
+    def size(self, dim: str):
+        return self.size_fn(dim, self.weight)
+
+    def size_fn(self, dim: str, weight):
+        if dim in self.weightshape:
+            return weight.shape[self.weightshape.index(dim)]
+        return None
+
+
+class Diagonal(NamedLinop):
+    def __init__(self, weight, ishape, oshape):
+        assert ishape == oshape, 'Diagonal linops must have matching input and output dimensions'
+        assert len(weight.shape) == len(ishape), 'All dimensions must be named'
+        super().__init__(ishape, oshape)
+        self.weight = weight
+
+    def forward(self, x):
+        return x * self.weight
+
+    def fn(self, x, /, weight):
+        return x * weight
+
+    def adj_fn(self, x, /, weight):
+        return x * torch.conj(weight)
+
+    def normal_fn(self, x, /, weight):
+        return x * torch.abs(weight) ** 2
+
+    def split_forward(self, ibatch, obatch):
+        weight = self.split_forward_fn(ibatch, obatch, self.weight)
+        return type(self)(weight, self.ishape, self.oshape)
+
+    def split_forward_fn(self, ibatch, obatch, /, weight):
+        assert ibatch == obatch, 'Diagonal linop must be split identically'
+        return weight[ibatch]
+
+    def size(self, dim: str):
+        return self.size_fn(dim, self.weight)
+
+    def size_fn(self, dim: str, weight):
+        if dim in self.ishape:
+            return weight.shape[self.ishape.index(dim)]
+        return None
+
 
 def slice_len(slc, n):
     """
     n: length of sequence slc is being applied to
     """
     return len(range(*slc.indices(n)))
+
 
 class Repeat(NamedLinop):
     """Unsqueezes and expands a tensor along dim
@@ -74,46 +162,4 @@ class Repeat(NamedLinop):
         if dim == self.oshape[self.dim]:
             return n_repeats
         return None
-
-
-class Diagonal(NamedLinop):
-    def __init__(self, weight, ishape, oshape):
-        assert ishape == oshape, 'Diagonal linops must have matching input and output dimensions'
-        assert len(weight.shape) == len(ishape), 'All dimensions must be named'
-        super().__init__(ishape, oshape)
-        self.weight = weight
-
-    def forward(self, x):
-        return x * self.weight
-
-    def fn(self, x, /, weight):
-        return x * weight
-
-    def adj_fn(self, x, /, weight):
-        return x * torch.conj(weight)
-
-    def normal_fn(self, x, /, weight):
-        return x * torch.abs(weight) ** 2
-
-    def split_forward(self, ibatch, obatch):
-        weight = self.split_forward_fn(ibatch, obatch, self.weight)
-        return type(self)(weight, self.ishape, self.oshape)
-
-    def split_forward_fn(self, ibatch, obatch, /, weight):
-        assert ibatch == obatch, 'Diagonal linop must be split identically'
-        return weight[ibatch]
-
-    def size(self, dim: str):
-        return self.size_fn(dim, self.weight)
-
-    def size_fn(self, dim: str, weight):
-        if dim in self.ishape:
-            return weight.shape[self.ishape.index(dim)]
-        return None
-
-class Dense(NamedLinop):
-    def __init__(self, mat, ishape, oshape):
-        super().__init__(ishape, oshape)
-        self.weight = weight
-
 
