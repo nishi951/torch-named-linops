0d4cf14b0bea7ff31c949c9d9bc8f790bf249494
Mark Nishimura <markn1@stanford.edu>
2024-11-19 22:07:47 -0800

    Adding multiprocess and persistent multiprocess

--- dev/cuda_streams/profile_streams.py
+++ dev/cuda_streams/profile_streams.py
@@ -6,7 +6,7 @@ from torch.profiler import profile, record_function, ProfilerActivity
 
 def main():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    B, M, N = 3, 100000, 1000
+    B, M, N = 3, 10000, 1000
     # Set up
     weight = torch.randn(B, M, N, device=device)
     A = Dense(weight, ("B", "M", "N"), ishape=("N",), oshape=("B", "M"))
@@ -23,12 +23,13 @@ def main():
         s0 = torch.cuda.Stream()
         s1 = torch.cuda.Stream()
         s2 = torch.cuda.Stream()
-        with torch.cuda.stream(s0):
-            y[0] = A_batch._linops[0](x)
-        with torch.cuda.stream(s1):
-            y[1] = A_batch._linops[1](x)
-        with torch.cuda.stream(s2):
-            y[2] = A_batch._linops[2](x)
+        for i in range(10):
+            with torch.cuda.stream(s0):
+                y[0] = A_batch._linops[0](x)
+            with torch.cuda.stream(s1):
+                y[1] = A_batch._linops[1](x)
+            with torch.cuda.stream(s2):
+                y[2] = A_batch._linops[2](x)
 
 
 if __name__ == "__main__":
