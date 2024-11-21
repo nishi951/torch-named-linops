870a0e8083adf380a76c33925a52b11afe7ff466
Mark Nishimura <markn1@stanford.edu>
2024-11-21 09:42:14 -0800

    More CUDA stream profiling (multiple GPU)

--- dev/cuda_streams/profile_streams.py
+++ dev/cuda_streams/profile_streams.py
@@ -5,31 +5,33 @@ from torch.profiler import profile, record_function, ProfilerActivity
 
 
 def main():
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    B, M, N = 3, 10000, 1000
+    device_idxs = [0, 1]
+    devices = [torch.device(f"cuda:{idx}") for idx in device_idxs]
+    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    B, M, N = 5, 100000, 10000
     # Set up
-    weight = torch.randn(B, M, N, device=device)
+    weight = torch.randn(B, M, N, device=devices[0])
     A = Dense(weight, ("B", "M", "N"), ishape=("N",), oshape=("B", "M"))
-    A.to(device)
-    A_batch = Batch(A, device, device, torch.float32, torch.float32, B=1)
-    A_batch = torch.compile(A_batch)
+    A.to(devices[0])
+    A_batch = Batch(A, devices[0], devices[0], torch.float32, torch.float32, B=1)
+    # Prepare streams
+    # streams = [torch.cuda.Stream(devices[d % len(devices)]) for d in range(B)]
+    default_streams = [torch.cuda.Stream(device) for device in devices]
+    streams = [default_streams[d % len(devices)] for d in range(B)]
+    for linop, stream in zip(A_batch._linops, streams):
+        linop.to(stream.device)
 
-    x = torch.randn(N, device=device)
-    y = torch.empty(B, M, device=device)
+    y = torch.empty(B, M, device=devices[0])
     with profile(
         activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
         on_trace_ready=lambda p: p.export_chrome_trace(f"./trace.json"),
     ) as prof:
-        s0 = torch.cuda.Stream()
-        s1 = torch.cuda.Stream()
-        s2 = torch.cuda.Stream()
         for i in range(10):
-            with torch.cuda.stream(s0):
-                y[0] = A_batch._linops[0](x)
-            with torch.cuda.stream(s1):
-                y[1] = A_batch._linops[1](x)
-            with torch.cuda.stream(s2):
-                y[2] = A_batch._linops[2](x)
+            for j, s in enumerate(streams):
+                with torch.cuda.stream(s):
+                    outputs = A_batch._linops[j](torch.randn(N, device=s.device))
+        for j, output in enumerate(outputs):
+            y[j] = output.to(y.device)
 
 
 if __name__ == "__main__":
