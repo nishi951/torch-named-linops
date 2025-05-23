335e0b3f3f6636c14baf57176da946bb5355243a
Mark Nishimura <mark@Marks-MacBook-Air.local>
2025-05-22 23:31:12 -0700

    fix: mac (apple silicon), reduced functionality
    Fixing dependencies for apple-silicon based-install with reduced
    functionality (no triton, no cupy)

--- src/torchlinops/functional/_unfold/casting.py
+++ src/torchlinops/functional/_unfold/casting.py
@@ -1,5 +1,21 @@
-import triton
-import triton.language as tl
+try:
+    import triton
+    import triton.language as tl
+
+    TRITON_ENABLED = True
+except ImportError:
+    from types import SimpleNamespace
+    from functools import wraps
+
+    # Replace all triton decorators with not implemented
+    def not_implemented_wrapper(*args, **kwargs):
+        raise NotImplementedError()
+
+    triton = SimpleNamespace()
+    triton.jit = lambda f: wraps(f)(not_implemented_wrapper)
+    triton.heuristics = lambda f: wraps(f)(not_implemented_wrapper)
+
+    TRITON_ENABLED = False
 
 __all__ = ["scalar_cast"]
 
