b059884a4ee21dbcf873d74d07cc29eeba407d69
Mark Nishimura <markn1@stanford.edu>
2025-05-29 01:58:02 -0700

    Using fake_triton, fake_tl in casting.py too

--- src/torchlinops/functional/_unfold/casting.py
+++ src/torchlinops/functional/_unfold/casting.py
@@ -4,16 +4,7 @@ try:
 
     TRITON_ENABLED = True
 except ImportError:
-    from types import SimpleNamespace
-    from functools import wraps
-
-    # Replace all triton decorators with not implemented
-    def not_implemented_wrapper(*args, **kwargs):
-        raise NotImplementedError()
-
-    triton = SimpleNamespace()
-    triton.jit = lambda f: wraps(f)(not_implemented_wrapper)
-    triton.heuristics = lambda f: wraps(f)(not_implemented_wrapper)
+    from torchlinops.utils import fake_triton as triton, fake_tl as tl
 
     TRITON_ENABLED = False
 
