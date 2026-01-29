b1a17fe95a74ee646d48271aac725566dd78f085
Mark Nishimura <nishimuramarky@gmail.com>
2026-01-29 02:32:57 -0800

    docs: WIP: auto-generated docs (needs better organization)

--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -14,7 +14,14 @@ import torchlinops.utils  # noqa: E402
 import torchlinops.alg  # noqa: E402
 
 
-def generate_pages(module, output_subdir, full_name_prefix):
+nav = mkdocs_gen_files.Nav()
+
+
+def fully_qualified_name(module, obj):
+    return f"{module.__name__}.{obj.__name__}"
+
+
+def generate_pages(module, output_subdir, nav):
     names = getattr(module, "__all__", None)
     if names is None:
         names = [n for n in dir(module) if not n.startswith("_")]
@@ -25,21 +32,34 @@ def generate_pages(module, output_subdir, full_name_prefix):
         # Skip modules
         if inspect.ismodule(obj):
             continue
+        if (not inspect.isfunction(obj)) and (not inspect.isclass(obj)):
+            continue
+
+        full_name = fully_qualified_name(module, obj)
 
         # Basic check to skip unrelated imports
         if hasattr(obj, "__module__") and obj.__module__:
             if not obj.__module__.startswith("torchlinops"):
                 continue
 
-        full_name = f"{full_name_prefix}.{name}"
-        filename = f"{full_name}.md"
-        file_path = Path("reference", "generated", output_subdir, filename)
+        # linops/some_linop
+        module_path = Path(output_subdir) / Path(*name.split("."))
 
-        with mkdocs_gen_files.open(file_path, "w") as f:
+        # reference/linops/some_linop.md
+        doc_path = module_path.with_suffix(".md")
+
+        # parts determines sidebar hierarchy
+        parts = tuple(full_name.split(".")[1:])
+        nav[parts] = doc_path.as_posix()
+
+        with mkdocs_gen_files.open(Path("reference") / doc_path, "w") as f:
             f.write(f"::: {full_name}\n")
 
 
 # Generate
-generate_pages(torchlinops, "linops", "torchlinops")
-generate_pages(torchlinops.alg, "algorithms", "torchlinops.alg")
-generate_pages(torchlinops.utils, "utils", "torchlinops.utils")
+generate_pages(torchlinops.linops, "linops", nav)
+generate_pages(torchlinops.alg, "algorithms", nav)
+generate_pages(torchlinops.utils, "utils", nav)
+
+with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
+    nav_file.writelines(nav.build_literate_nav())
